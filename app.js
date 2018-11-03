bda8c539e8036fc7a5a12b0f026fc8f74d74c083
sajicode <sajioloye@gmail.com>
2018-11-04 00:14:49 +0100

    set up handlebars layout and implement in app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bps = require('body-parser');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const morgan = require('morgan');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
@@ -23,20 +22,25 @@ passportConfig();
 
 app.set("port", process.env.PORT || 3100);
 
-app.engine('handlebars', exphbs());
+// set up views engine
+app.set("views", path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'layout'
+}));
 app.set('view engine', 'handlebars');
 
-// app.set("views", path.join(__dirname, "views"));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan("dev"));
-app.use(cors());
 
 app.use(bps.json());
 app.use(bps.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
+
+// Express session
 app.use(session({
   secret: process.env.SECRET,
   resave: true,
