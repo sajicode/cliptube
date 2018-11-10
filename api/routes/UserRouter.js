224522c60264c4514c1c13339e5463c30925de1a
sajicode <sajioloye@gmail.com>
2018-11-11 00:28:59 +0100

    change email re-rout link to heroku link

--- api/routes/UserRouter.js
+++ api/routes/UserRouter.js
@@ -41,8 +41,9 @@ router.post("/register", function (req, res, next) {
   let firstname = req.body.firstname;
   let username = req.body.username;
   let password = req.body.password;
-  let port = `http://localhost:${process.env.PORT}` || `cliptube.herokuapp.com`;
-  let message = `<h3 style="text-align:center; color: blue;"><a href='${port}/login'>Click here</a> to login</h3>`;
+  // let port = `http://localhost:${process.env.PORT}` || `cliptube.herokuapp.com`;
+
+  let message = `<h3 style="text-align:center; color: blue;"><a href='cliptube.herokuapp.com/login'>Click here</a> to login</h3>`;
 
   // validation
   req.checkBody('firstname', 'Name is required').notEmpty();
