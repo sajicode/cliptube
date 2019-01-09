3163a2ecbaa1be940d9b9b1561f23386e6d7e5b9
sajicode <sajioloye@gmail.com>
2019-01-09 01:13:38 +0100

    modify nodemailer config

--- config/sendmail.js
+++ config/sendmail.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 
 function sendMail(address, message) {
   let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
