9d388e198c08282e1315c479115682ea69aa5dbc
sajicode <sajioloye@gmail.com>
2019-01-09 01:48:43 +0100

    set yahoo mail as mail host

--- config/sendmail.js
+++ config/sendmail.js
@@ -2,9 +2,9 @@ const nodemailer = require('nodemailer');
 
 function sendMail(address, message) {
   let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    host: 'smtp.mail.yahoo.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
